6e6302536f4495d913de0b124ac72bbfb533ce58
Kent Ritchie <kintobean@github.com>
2023-12-06 14:02:04 -0700

    mongo db linked for export

--- R/mod-traininglog.R
+++ R/mod-traininglog.R
@@ -15,6 +15,13 @@ modTrainingLogUI <- function(id) {
       actionButton(ns("addButton"), "Add row", icon = icon('plus')),
       actionButton(ns("dupeButton"), "Duplicate selected row(s)", icon = icon('plus')),
       actionButton(ns("delButton"), "Remove selected row(s)", icon = icon('minus')),
+    ),
+    div(
+      textInput(ns("mongousername"), "Username"),
+      textInput(ns("mongopassword"), "Password")
+    ),
+    div(
+      actionButton(ns("exportmongo"),"Export to Mongo")
     )
   )
 }
@@ -95,6 +102,85 @@ modTrainingLogServer <- function(id) {
         rownames = FALSE,
         width = '80%'
       )
+    })
+
+    observeEvent(input$exportmongo, {
+
+      options(mongodb = list(
+        "host" = "cluster0.lta9lc0.mongodb.net",
+        "username" = input$mongousername,
+        "password" = input$mongopassword
+      ))
+
+      databaseName <- "traininglogdb"
+      collectionName <- "traininglogs"
+
+      saveData <- function(data) {
+        # Connect to the database
+        db <- mongolite::mongo(collection = collectionName,
+                               url = sprintf(
+                                 "mongodb+srv://%s:%s@%s/%s",
+                                 options()$mongodb$username,
+                                 options()$mongodb$password,
+                                 options()$mongodb$host,
+                                 databaseName
+                               ),
+                               options = mongolite::ssl_options(weak_cert_validation = TRUE))
+        # Insert the data into the mongo collection as a data.frame
+        data <- as.data.frame(t(data))
+        db$insert(data)
+      }
+
+      loadData <- function() {
+        # Connect to the database
+        db <- mongolite::mongo(collection = collectionName,
+                               url = sprintf(
+                                 "mongodb+srv://%s:%s@%s/%s",
+                                 options()$mongodb$username,
+                                 options()$mongodb$password,
+                                 options()$mongodb$host,
+                                 databaseName
+                               ),
+                               options = mongolite::ssl_options(weak_cert_validation = TRUE))
+        # Read all the entries
+        data <- db$find()
+        data
+      }
+
+      req(input$mongousername)
+      req(input$mongopassword)
+
+      tryExport <- function() {
+        result <- tryCatch({
+          saveData(rv_table())
+          "success"
+        }, warning = function(warning_message) {
+          cat("Warning: ", warning_message$message, "\n")
+          "warning"
+        }, error = function(error_message) {
+          cat("Error: ", error_message$message, "\n")
+          "error"
+        }, finally = {
+          cat("done\n")
+        })
+
+        return(result)
+      }
+
+      # Example usage
+      result <- tryExport()
+
+      # Check the result
+      if (result == "success") {
+        cat("The operation was successful!\n")
+      } else if (result == "warning") {
+        cat("The operation produced a warning.\n")
+      } else if (result == "error") {
+        cat("The operation encountered an error.\n")
+      } else {
+        cat("Unknown result.\n")
+      }
+
     })
 
   })
